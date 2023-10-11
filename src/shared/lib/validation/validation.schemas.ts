83acb4f05b44b7d9575382ecbb05496b06a3ca91
magtm <uporoff.maxim@yandex.ru>
2023-10-11 11:52:16 +0600

    fix: :bug: fix naming

--- src/shared/lib/validation/validation.schemas.ts
+++ src/shared/lib/validation/validation.schemas.ts
@@ -1,5 +1,5 @@
 import {z} from "zod"
 import {INCORRECT_EMAIL, REQUIRED_FIELD} from './validation.errors';
 
-export const EMAIL_SCHEMA = z.string({required_error: REQUIRED_FIELD}).nonempty(INCORRECT_EMAIL).email(INCORRECT_EMAIL).trim()
-export const NAMING_SCHEMA = z.string({required_error: REQUIRED_FIELD}).nonempty(INCORRECT_EMAIL).trim()
+export const emailSchema = z.string({required_error: REQUIRED_FIELD}).nonempty(INCORRECT_EMAIL).email(INCORRECT_EMAIL).trim()
+export const namingSchema = z.string({required_error: REQUIRED_FIELD}).nonempty(INCORRECT_EMAIL).trim()
